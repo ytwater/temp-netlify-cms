0cb598b67e2280e0fb7f3ed98ecca474631a6be8
Alex Chong <alex.chong@psg-solutions.com>
2020-03-13 15:01:29 -0700

    no cache public

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,12 +5,7 @@ module.exports = {
       "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution."
   },
   plugins: [
-    {
-      resolve: "gatsby-plugin-netlify-cache",
-      options: {
-        cachePublic: true
-      }
-    },
+    "gatsby-plugin-netlify-cache",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sass",
     {
