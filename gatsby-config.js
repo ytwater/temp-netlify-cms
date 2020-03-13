20fa8a287ec78d29cc26c583c79f886f60a374bb
Alex Chong <alex.chong@psg-solutions.com>
2020-03-13 15:14:16 -0700

    test push

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,6 @@ module.exports = {
       "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution."
   },
   plugins: [
-    "gatsby-plugin-netlify-cache",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sass",
     {
